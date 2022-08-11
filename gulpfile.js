947dea7a9c2d8ca5009c2785b1041837cd867c37
Sean <18058799212@163.com>
2022-08-11 11:00:34 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,22 @@
-const { src, dest } = require('gulp')
-var clean = require('gulp-clean')
-var uglify = require('gulp-uglify')
+const gulp = require('gulp')
+const { src, dest, parallel, series } = gulp
+const clean = require('gulp-clean')
+const uglify = require('gulp-uglify')
 const babel = require('gulp-babel')
 
-exports.default = function () {
-  return src('src/*.js')
-    .pipe(babel())
-    .pipe(uglify())
-    .pipe(clean())
-    .pipe(dest('dist/'))
+// clean
+function cleanDist() {
+  return src('dist/', { read: false, allowEmpty: true }).pipe(clean())
 }
+
+// js
+function bulidjs() {
+  return src('src/*.js').pipe(babel()).pipe(uglify()).pipe(dest('dist/'))
+}
+
+// json wxss wxml
+function bulidjson(cb) {
+  return src(['src/*.json', 'src/*.wxml', 'src/*.wxss']).pipe(dest('dist/'))
+}
+
+exports.default = series(cleanDist, parallel(bulidjs, bulidjson))
